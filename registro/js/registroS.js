2b8204748bd292807cde07b5b9d9c43e2befd6a8
MiguelAnchico <98991880+MiguelAnchico@users.noreply.github.com>
2022-04-07 17:08:59 -0500

    Add files via upload

--- registro/js/registroS.js
+++ registro/js/registroS.js
@@ -1,7 +1,40 @@
-let txt_nombre = $("nombre").val();
-let txt_email = $("correo").val();
-let txt_contraseña = $("contraseña").val();
-let txt_edad = $("edad").val();
-let txt_pais = $("pais").val();
-let txt_foto = $("foto_perfil").val();
-let txt_datos = $("datos_perfil").val();
+
+
+function registrar() {
+    let txt_nombre = $("#nombre").val();
+    let txt_correo = $("#correo").val();
+    let txt_contrasena = $("#contrasena").val();
+    let txt_edad = $("#edad").val();
+    let txt_pais = $("#pais").val();
+    
+    $.ajax({
+        url:'https://pruebauao.azurewebsites.net/streamers',   // url
+        dataType: 'text/json',
+        type: 'post',
+        contentType: 'application/x-www-form-urlencoded',
+        data: {
+            nombre: txt_nombre,
+            correo: txt_correo,
+            password: txt_contrasena,
+            edad: txt_edad,
+            pais: txt_pais,
+            foto_perfil: "",
+            datos_perfil: "",
+        },
+        success: function( data, textStatus, jQxhr ){
+            correcto(true);
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+        }
+
+        
+    });
+    
+    
+}
+
+function correcto(a){
+    if(a == true){
+        window.location.href = "http://127.0.0.1:5500/homeStreamer/homestreamer.html";
+    }
+}
