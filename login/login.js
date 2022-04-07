fdba10a28752dede278758b3a044fd96c48c920e
Germanny <duglaslezama@gmail.com>
2022-04-06 23:18:32 -0500

    login full

---
+++ login/login.js
@@ -0,0 +1,8 @@
+let email = document.getElementById("email").value;
+let contraseña = document.getElementById("contraseña").value;
+let Streamer = document.getElementById("Streamer").value;;
+let Colaborador = document.getElementById("Colaborador").value;
+console.log(email);
+console.log(contraseña);
+console.log(Streamer);
+
